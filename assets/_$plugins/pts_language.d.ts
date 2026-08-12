65ade64ce675c1870265ee7e1f74a0724f26351d
pTSern <tsern.dev@gmail.com>
2026-08-12 18:03:52 +0700

    push

--- assets/_$plugins/pts_language.d.ts
+++ assets/_$plugins/pts_language.d.ts
@@ -2,7 +2,7 @@ declare namespace pTS {
     export namespace languages {
         const _$langs = ["en", "vi"] as const;
         export type ELang = typeof _$langs[number];
-        const _$keys = ["get", "reward", "double", "daily", "areas", "room", "shop", "home", "jigsaw", "rank", "level"] as const;
+        const _$keys = ["get", "reward", "double", "daily", "areas", "room", "shop", "home", "jigsaw", "rank", "level", "day"] as const;
         export type EKey = typeof _$keys[number];
         export const path: string;
         export const ELang: Record<string, string>;
