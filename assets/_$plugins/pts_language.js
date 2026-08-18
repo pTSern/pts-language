f194fbeff0c4a0a8db161219af8da45b242300b7
pTSern <tsern.dev@gmail.com>
2026-08-18 10:12:17 +0700

    push

--- assets/_$plugins/pts_language.js
+++ assets/_$plugins/pts_language.js
@@ -1,4 +1,4 @@
-const _$path = "game";
+const _$path = "lang";
 const _$langs = {"__enums__":null,"en":"en","vi":"vi"};
 const _$container = {"__enums__":null,"get":"get","reward":"reward","double":"double","daily":"daily","areas":"areas","room":"room","shop":"shop","home":"home","jigsaw":"jigsaw","rank":"rank","level":"level","day":"day","claim":"claim","claimed":"claimed","comming_soon":"comming_soon","daily_reward":"daily_reward","setting":"setting","quit":"quit","wait":"wait","please":"please","dyn_not_enough_@amount_@money":"dyn_not_enough_@amount_@money","notice":"notice","dyn_watch_ads_for_@reward":"dyn_watch_ads_for_@reward","dyn_ur_missing_@amount_ads_ticket_for_this_reward":"dyn_ur_missing_@amount_ads_ticket_for_this_reward","ok":"ok","no":"no"};
 const _$count = 2;
@@ -8,7 +8,7 @@ function _$has(key) {
 }
 
 function _$load(callback) {
-    const _ = "$language";
+    const _ = "";
     for (const _lang in _$langs) {
         if (_lang === '__enums__') continue;
         const _sub = _ ? (_ + '/' + _lang) : _lang;
